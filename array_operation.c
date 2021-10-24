2a0a4f8b2e41fff845b8505c385d3b9ba2053afe
Shivamcrash <66561318+Shivamcrash@users.noreply.github.com>
2021-10-25 00:36:41 +0530

    version1

---
+++ array_operation.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+int i,sum,max,min,avg,size;
+
+printf("Enter array length : \n");
+scanf("%d",&size);
+
+int ara[size];
+
+printf("Enter array elements:\n");
+for(i = 0; i < size;  i++){
+scanf("%d", &ara[i]);
+}
+
+
+
+sum=0;
+max=ara[0];
+min=ara[0];
+for(i = 0; i < size; i++){
+printf("Your array elements are %d \n",ara[i]);
+
+sum+=ara[i];
+if(ara[i] > max){
+max = ara[i];
+}
+if(ara[i] < min){
+min = ara[i];
+}
+}
+avg=sum/size;
+
+printf("Sum is %d\n",sum);
+printf("Average is %d\n",avg);
+printf("Max number is %d\n",max);
+printf("Min number is %d\n",min);
+
+
+return 0;
+}
